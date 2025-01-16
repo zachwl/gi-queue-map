a69a66093439f4525988ad42f4ccf26c0c8409cb
zachwl <88093069+zachwl@users.noreply.github.com>
2025-01-15 21:42:11 -0500

    fix dates isone

--- scripts/isone.py
+++ scripts/isone.py
@@ -49,8 +49,8 @@ def getISONEQueue():
         # This system is not perfect, so in the future, I might add a way to split that projects generation evenly across all counties it is located in
         isone_active_projects['County'] = isone_active_projects['County'].str.split('/').str[0]
         # Trim down the values for queue date and in service date
-        isone_active_projects['Requested'] = pd.to_datetime(isone_active_projects['Requested']).dt.date
-        isone_active_projects['Op Date'] = pd.to_datetime(isone_active_projects['Op Date']).dt.date
+        isone_active_projects['Requested'] = isone_active_projects['Requested'].astype(str).str[:10]
+        isone_active_projects['Op Date'] = isone_active_projects['Op Date'].astype(str).str[:10]
 
         #### Standardize Columns ####
 
